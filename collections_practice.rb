713a8da2f151a355bcebcf9d7ca1dfe80ce08a8a
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-26 00:11:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
 
+def sort_array_asc(int)
+  int.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(int)
+  int.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort {|short, long| short.length <=> long.length}
+end
+
+def swap_elements(array)
+   array[1], array[2] = array[2], array[1]
+   return array
+end
+
+def reverse_array(int)
+  int.reverse 
+end
